3e06232ca2ad3fb5f5c901aec2a4e057e6337e76
aican0830 <aican0830@163.com>
2012-02-24 17:23:45 +0800

    ci集成doctrine

---
+++ cidoctrine/application/models/User.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace models;
+
+/**
+ * models\User
+ *
+ * @Table(name="user")
+ * @Entity
+ */
+class User
+{
+    /**
+     * @var integer $id
+     *
+     * @Column(name="id", type="integer", precision=0, scale=0, nullable=false, unique=false)
+     * @Id
+     * @GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var string $username
+     *
+     * @Column(name="username", type="string", length=32, precision=0, scale=0, nullable=true, unique=true)
+     */
+    private $username;
+
+    /**
+     * @var string $password
+     *
+     * @Column(name="password", type="string", length=64, precision=0, scale=0, nullable=true, unique=false)
+     */
+    private $password;
+
+    /**
+     * Get id
+     *
+     * @return integer $id
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set username
+     *
+     * @param string $username
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+    }
+
+    /**
+     * Get username
+     *
+     * @return string $username
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * Set password
+     *
+     * @param string $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * Get password
+     *
+     * @return string $password
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+}
